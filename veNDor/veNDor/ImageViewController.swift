c7daf60e4eae38b9348ac366ab7f813fa2e1365b
J West <mwest@js-macbook-air.dhcp.nd.edu>
2018-04-14 15:56:35 -0400

    Maggie changes

---
+++ veNDor/veNDor/ImageViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ImageViewController.swift
+//  veNDor
+//
+//  Created by West   on 4/14/18.
+//  Copyright © 2018 veNDor Team. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet weak var imagepicker: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
