98b6ebcbe7134414d12a74c2d7b524dfac3ff571
J West <mwest@js-macbook-air.dhcp.nd.edu>
2018-04-22 03:08:23 -0400

    ImageViewController upload function

--- veNDor/veNDor/ImageViewController.swift
+++ veNDor/veNDor/ImageViewController.swift
@@ -14,6 +14,8 @@ class ImageViewController: UIViewController, UIImagePickerControllerDelegate, UI
     @IBOutlet weak var imagepicker: UIImageView!
     @IBOutlet weak var ItemDescription: UITextField!
     @IBOutlet weak var ItemPrice: UITextField!
+    @IBAction func UploadPressed(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
